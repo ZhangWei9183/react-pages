8b0c197f1def06d22a35a522f046c42a7b693a6b
ZhangWei9183 <ZhangWei9183@outlook.com>
2025-10-29 15:55:33 +0000

    Refactor Hello component to return a greeting

--- js/apps/Hello.jsx
+++ js/apps/Hello.jsx
@@ -1,14 +1,4 @@
-import React from 'react';
-
-
-const App = (props) =>
+const Hello = () =>
 {
-    return (
-        <div className='App'>
-        <h1>Hello React.</h1>
-        <h2>Start editing to see some magic happen!</h2>
-        </div>
-    )
+    return <h2>Hello, React</h2>
 }
-
-export { App }
